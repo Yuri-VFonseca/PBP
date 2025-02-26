95cbf5b4f4fa0e543d70363685e76b7d73063361
Yuri <yurifonseca.comercial@gmail.com>
2025-02-26 10:39:58 -0300

    up

---
+++ trabalho pratico/main.py
@@ -0,0 +1,35 @@
+'''
+Fazer CRUD -  Create, Read, Update and Delete
+Usuários - Controle de acesso (usuario (ver itens), gerente (ver, criar, reduzir e aumentar n de itens e colocar novos preços) e funcionário (ver e reduzir ou aumentar n de itens)
+Organizar em um doc (.csv)
+Produtos ou serviços - armazernar em um .csv separado, aplicar crud
+'''
+import random
+import csv
+#funcoes - produtos
+produtos = []
+#create
+def create_produto(): 
+    with open('produtos.csv', 'a', n)
+
+#read
+
+#update
+
+#delete
+
+#usuarios
+#create 
+
+#read
+
+#update
+
+#delete
+
+#cliente (ver produto, alterar suas info)
+
+#funcionario (ver, dar baixa e aumentar, atualizar clientes e criar)
+
+#gerente (todo o restante + cadastrar novos produtos e alterar preços, CRUD clientes)
+
